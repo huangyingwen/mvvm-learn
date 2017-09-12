65aaa93aa5e2bffa3a75bfd6d48b67c596a1ff96
huangyingwen <hyw.huangyingwen@gmail.com>
2017-09-12 16:41:01 +0800

    karma PhantomJS 配置

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'node_modules/babel-polyfill/dist/polyfill.js',
       'src/**/*.js',
       'test/**/*.js'
     ],
@@ -95,7 +96,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
